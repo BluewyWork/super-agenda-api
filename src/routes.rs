3d23b73f0b9a2c7096cb4601260494ec0ba53ebf
BluewyWork <bluewy.s.work@gmail.com>
2024-03-08 21:23:04 +0100

    feat(routes): add user registration route

--- src/routes.rs
+++ src/routes.rs
@@ -3,5 +3,10 @@ use axum::{routing::post, Router};
 use crate::controllers;
 
 pub fn auth_user() -> Router {
-   Router::new().route("/auth/user/login", post(controllers::user::auth::login))
+   Router::new()
+      .route("/auth/user/login", post(controllers::user::auth::login))
+      .route(
+         "/auth/user/register",
+         post(controllers::user::auth::register),
+      )
 }
