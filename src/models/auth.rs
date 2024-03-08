01d952e15e25cdead36c655fa3a1263314539712
BluewyWork <bluewy.s.work@gmail.com>
2024-03-08 11:46:27 +0100

    feat(models): create register payload struct

--- src/models/auth.rs
+++ src/models/auth.rs
@@ -5,3 +5,10 @@ pub struct LoginPayload {
    pub username: String,
    pub password: String,
 }
+
+#[derive(Debug, Deserialize)]
+pub struct RegisterPaylaod {
+   pub username: String,
+   pub email: String,
+   pub password: String,
+}
