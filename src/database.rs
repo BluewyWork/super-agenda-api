acecc120846397590007ea5fc069012430fc63a4
BluewyWork <bluewy.s.work@gmail.com>
2024-03-08 21:19:23 +0100

    feat(database): print errors

--- src/database.rs
+++ src/database.rs
@@ -1,26 +1,40 @@
 use mongodb::{options::ClientOptions, Client, Database};
 
 pub async fn mongodb_connection() -> Result<Database, ()> {
-   let env_address = match std::env::var("MONGO_URL") {
+   let env_adress_name = "MONGO_URL";
+   let env_address_value = match std::env::var(env_adress_name) {
       Ok(env) => env,
-      Err(_) => return Err(()),
+      Err(_) => {
+         println!("env: {} not found", env_adress_name);
+         return Err(());
+      },
    };
 
-   let client_options = match ClientOptions::parse(env_address).await {
+   let client_options = match ClientOptions::parse(&env_address_value).await {
       Ok(opt) => opt,
-      Err(_) => return Err(()),
+      Err(_) => {
+         println!("mongodb: unable to parse -> {}", env_address_value);
+         return Err(());
+      },
    };
 
    let client = match Client::with_options(client_options) {
       Ok(client) => client,
-      Err(_) => return Err(()),
+      Err(_) => {
+         println!("mongodb: unable to obtain client");
+         return Err(());
+      },
    };
 
-   let env_database = match std::env::var("MONGO_DB") {
+   let env_database_name = "MONGO_DB";
+   let env_database_value = match std::env::var(env_database_name) {
       Ok(env) => env,
-      Err(_) => return Err(()),
+      Err(_) => {
+         println!("env: {} not found", env_database_name);
+         return Err(());
+      },
    };
-   let db = client.database(&env_database);
+   let db = client.database(&env_database_value);
 
    Ok(db)
 }
