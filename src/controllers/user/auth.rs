7d4434afb80ef866c209f374599db3f83051287b
BluewyWork <bluewy.s.work@gmail.com>
2024-03-08 11:56:43 +0100

    refactor(controllers): draft authentication stuff

--- src/controllers/user/auth.rs
+++ src/controllers/user/auth.rs
@@ -1,3 +1,33 @@
-pub async fn login() {
-   todo!()
+use axum::{http::StatusCode, Json};
+
+use crate::{
+   database::mongodb_connection,
+   models::{
+      api::Answer,
+      auth::{LoginPayload, RegisterPaylaod},
+   },
+};
+
+pub async fn login(payload: Json<LoginPayload>) -> Answer {
+   if payload.username != "demo" || payload.password != "demo" {
+      return Answer {
+         json: "Invalid Credentials".into(),
+         status: StatusCode::UNAUTHORIZED,
+         ok: true,
+      };
+   }
+
+   Answer {
+      json: "Login Successful".into(),
+      status: StatusCode::OK,
+      ok: true,
+   }
+}
+
+pub async fn register(payload: RegisterPaylaod) -> Answer {
+   Answer {
+      json: "User Registered Sucessfully".into(),
+      status: StatusCode::OK,
+      ok: true,
+   }
 }
