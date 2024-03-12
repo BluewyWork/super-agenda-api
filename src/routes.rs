a7d14938f62715a3bed3de08470b0cdc0592c7f0
BluewyWork <bluewy.s.work@gmail.com>
2024-03-12 14:45:37 +0100

    refactor(routes): break guest routes into smaller parts

--- src/routes.rs
+++ src/routes.rs
@@ -1,9 +1,20 @@
-use axum::{routing::post, Router};
+use axum::{
+   routing::{get, post},
+   Router,
+};
 
-use crate::controllers;
+use crate::{controllers, middleware::guest_middleware};
 
 pub fn user_routes() -> Router {
+   let user_auth_route = Router::new()
+      .route("/login", post(controllers::user::auth::login))
+      .route("/register", post(controllers::user::auth::register));
+
+   let user_test_route = Router::new()
+      .route("/hi", get(|| async { println!("hi") }))
+      .layer(axum::middleware::from_fn(guest_middleware));
+
    Router::new()
-      .route("/auth/login", post(controllers::user::auth::login))
-      .route("/auth/register", post(controllers::user::auth::register))
+      .nest("/auth", user_auth_route)
+      .nest("/test", user_test_route)
 }
