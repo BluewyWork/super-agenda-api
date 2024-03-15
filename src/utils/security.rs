4e8959fcd03838f5f894e091c809e346d0567ff2
BluewyWork <bluewy.s.work@gmail.com>
2024-03-15 17:18:02 +0100

    feat(utils): print error messages

--- src/utils/security.rs
+++ src/utils/security.rs
@@ -1,9 +1,15 @@
 use bcrypt;
 
 pub fn hash_password(password: String) -> Result<String, ()> {
-   bcrypt::hash(password, 10).map_err(|_| ())
+   bcrypt::hash(password, 10).map_err(|_| {
+      println!("api: unable to hash password");
+      ()
+   })
 }
 
 pub fn verify_password(password: String, hash: &str) -> Result<bool, ()> {
-   bcrypt::verify(password, hash).map_err(|_| ())
+   bcrypt::verify(password, hash).map_err(|_| {
+      println!("api: unable to verify hashed password");
+      ()
+   })
 }
