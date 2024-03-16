b3978d91f6de5d8c05d8c051eda3454efb093d99
BluewyWork <bluewy.s.work@gmail.com>
2024-03-16 15:34:42 +0100

    feat(schemas): draft task schema

--- src/schemas.rs
+++ src/schemas.rs
@@ -1,3 +1,4 @@
+use mongodb::bson::DateTime;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -5,4 +6,43 @@ pub struct User {
    pub username: String,
    pub email: String,
    pub password: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Task {
+   pub object_id: String,
+   pub title: Option<String>,
+   pub description: Option<String>,
+   pub status: Status,
+   pub priority: Priority,
+   pub tags: Option<Vec<String>>,
+   pub planning: Option<Planning>,
+   pub metrics: Option<Metrics>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub enum Status {
+   NotStarted,
+   Ongoing,
+   Completed,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub enum Priority {
+   High,
+   Normal,
+   Low,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Planning {
+   initiation: Option<DateTime>,
+   deadline: Option<DateTime>,
+   estimated_duration_in_minutes: Option<u32>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Metrics {
+   date_time_when_status_is_started: Option<DateTime>,
+   date_time_when_status_is_completed: Option<DateTime>,
 }
