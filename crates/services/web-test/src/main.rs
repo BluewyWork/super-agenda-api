edf644fa88669301e250ac5116fd81c874aabd7b
BluewyWork <bluewy.s.work@gmail.com>
2024-04-25 19:40:43 +0200

    chore(web-test)

--- crates/services/web-test/src/main.rs
+++ crates/services/web-test/src/main.rs
@@ -1,7 +1,22 @@
-use lib_database::models::database::DatabaseManager;
+use std::str::FromStr;
+
+use lib_database::models::{database::DatabaseManager, tables::{user::UserTable, user_data::UserDataTable}};
 use lib_utils::constants::{MONGO_DB, MONGO_URI};
+use mongodb::bson::oid::ObjectId;
 
 #[tokio::main]
 async fn main() {
-   todo!()
+   let database_manager = DatabaseManager::from(&MONGO_URI, &MONGO_DB).await.unwrap();
+
+   let user_table = UserTable::from(database_manager.clone());
+
+   let user = user_table.find_user_from_object_id(ObjectId::from_str("6622b619c23a20b74fee6c57").unwrap()).await.unwrap();
+
+   println!("{}", user.username);
+
+   let user_data_table = UserDataTable::from(database_manager);
+
+   let task_list = user_data_table.get_task_list(ObjectId::from_str("6622b619c23a20b74fee6c57").unwrap()).await.unwrap();
+
+   println!("{:?}", task_list);
 }
